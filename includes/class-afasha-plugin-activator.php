d06e801259fadc5f5685615f98c5a3311f83e7a7
Isakiye Afasha <afashaisakiye@gmail.com>
2021-02-10 10:20:37 +0300

    First commit

---
+++ includes/class-afasha-plugin-activator.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       iamafasha.com
+ * @since      1.0.0
+ *
+ * @package    Afasha_Plugin
+ * @subpackage Afasha_Plugin/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Afasha_Plugin
+ * @subpackage Afasha_Plugin/includes
+ * @author     Isakiye Afasha <me@iamafasha.com>
+ */
+class Afasha_Plugin_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+	}
+
+}
